9d22c10888301b448e8ebc0aca910dbabd66f425
Marcin Ostaszewski <marcinostaszewski@gmail.com>
2022-02-20 20:26:12 +0100

    add and modify playersData

---
+++ src/store/playersData.js
@@ -0,0 +1,89 @@
+export const playersData = [{
+        id: "player1",
+        values: {
+            position: {
+                x: 200,
+                y: 200,
+            },
+            angle: -45,
+            speed: {
+                x: 0,
+                y: 0,
+            },
+            rotationSpeed: 0,
+            backgroundColor: "blue",
+        },
+        keys: {
+            frwd: "KeyW",
+            back: "KeyS",
+            left: "KeyA",
+            rght: "KeyD",
+        },
+    },
+    {
+        id: "player2",
+        values: {
+            position: {
+                x: 800,
+                y: 200,
+            },
+            angle: 45,
+            speed: {
+                x: 0,
+                y: 0,
+            },
+            rotationSpeed: 0,
+            backgroundColor: "red",
+        },
+        keys: {
+            frwd: "BracketLeft",
+            back: "Quote",
+            left: "Semicolon",
+            rght: "Backslash",
+        },
+    },
+    {
+        id: "player3",
+        values: {
+            position: {
+                x: 800,
+                y: 600,
+            },
+            angle: 135,
+            speed: {
+                x: 0,
+                y: 0,
+            },
+            rotationSpeed: 0,
+            backgroundColor: "green",
+        },
+        keys: {
+            frwd: "ArrowUp",
+            back: "ArrowDown",
+            left: "ArrowLeft",
+            rght: "ArrowRight",
+        },
+    },
+    {
+        id: "player4",
+        values: {
+            position: {
+                x: 200,
+                y: 600,
+            },
+            angle: 225,
+            speed: {
+                x: 0,
+                y: 0,
+            },
+            rotationSpeed: 0,
+            backgroundColor: "brown",
+        },
+        keys: {
+            frwd: "KeyJ",
+            back: "KeyM",
+            left: "KeyN",
+            rght: "Comma",
+        },
+    },
+];
