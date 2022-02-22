a9e505f48052e6e5bc96a06f8a0d4dfaffca3061
Marcin Ostaszewski <marcinostaszewski@gmail.com>
2022-02-22 19:13:04 +0100

    clean up Player code

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,17 +1,13 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 import { StyledPlayer } from "./Player.styles";
 
-const Player = () => {
-    const dispatch = useDispatch();
+const Players = () => {
     const playersData = useSelector(state => state.playersData);
 
-    const players = playersData.map((player, index) => {
-        console.log(player.values, index);
+    return playersData.map((player, index) => {
         return <StyledPlayer values={player.values} key={index} id={index} />;
     });
-
-    return players;
 };
 
-export default Player;
+export default Players;
