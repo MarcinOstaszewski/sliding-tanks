d3b554d895c9c1a74b6515680b5566844e74abba
Marcin Ostaszewski <marcinostaszewski@gmail.com>
2022-02-22 19:19:19 +0100

    add keys actions to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import "./App.css";
-import Player from "./components/Player/Player";
+import { useSelector, useDispatch } from "react-redux";
+import { keysActions } from "./store/index";
+import Players from "./components/Players/Players";
 
 function App() {
+    const dispatch = useDispatch();
+    const keysPressed = useSelector(state => state.keysPressed);
+
+    const onKeyDown = e => {
+        dispatch({ type: keysActions.addKey(), payload: e.code });
+    };
+
+    const onKeyUp = e => {
+        console.log(keysPressed);
+        dispatch({ type: keysActions.delKey(), payload: e.code });
+    };
+
+    document.addEventListener("keydown", onKeyDown);
+    document.addEventListener("keyup", onKeyUp);
+
     return (
         <div className="App">
-            <Player />
+            <Players />
         </div>
     );
 }
