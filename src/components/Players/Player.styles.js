c8b302685dbbcce7d2dd82d9d50ec44a27650dc9
Marcin Ostaszewski <marcinostaszewski@gmail.com>
2022-02-28 19:52:55 +0100

    improve Players styles

--- src/components/Players/Player.styles.js
+++ src/components/Players/Player.styles.js
@@ -1,20 +1,31 @@
 import styled from "styled-components";
 
-export const StyledPlayer = styled.div `
+export const StyledPlayer = styled.div`
     width: 25px;
     height: 25px;
+    border-radius: 9px 9px 5px 5px;
     background-color: ${props => props.values.backgroundColor};
     position: absolute;
     transform: rotate(${props => props.values.angle}deg);
     left: ${props => props.values.position.x}px;
     top: ${props => props.values.position.y}px;
+    &::before,
     &:after {
         content: "";
         position: absolute;
-        top: 2px;
-        left: 5px;
-        width: 15px;
-        height: 4px;
         background-color: #fff;
+    }
+    &:before {
+        top: 10px;
+        left: 6px;
+        width: 14px;
+        height: 10px;
+        border-radius: 3px;
+    }
+    &:after {
+        top: 1px;
+        left: 11px;
+        width: 4px;
+        height: 10px;
     }
 `;
