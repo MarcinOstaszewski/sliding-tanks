363e34350b05e9b3ff0d65613fb712729ddb9b5e
Marcin Ostaszewski <marcinostaszewski@gmail.com>
2022-03-11 16:53:13 +0100

    add constants for players and game screen

---
+++ src/helpers/consts.js
@@ -0,0 +1,21 @@
+const consts = {
+    PLAYER_RADIUS: 12,
+    FRAME_INTERVAL: 30,
+    DEG_TO_RAD: Math.PI / 180,
+    RAD_TO_DEG: 180 / Math.PI,
+    SPEED_DELTA: 1,
+    SPEED_MIN: .01,
+    SPEED_MAX: 10,
+    SPEED_DELTA_MAX: 5,
+    SPEED_FRICTION: .98,
+    SPEED_FACTOR_AFTER_COLLISION: 0.9,
+    ROTATION_DELTA: .2,
+    ROTATION_FRICTION: .96,
+    ROTATION_DELTA_MAX: 5.5,
+    ROTATION_DELTA_MIN: .1,
+    WINDOW_HEIGHT: window.innerHeight,
+    WINDOW_WIDTH: window.innerWidth,
+    BOUNCE_FACTOR: -0.33,
+}
+
+export default consts;
