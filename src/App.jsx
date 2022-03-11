d6604f20ad6decc9f79b47f1e14e638523d65c36
Marcin Ostaszewski <marcinostaszewski@gmail.com>
2022-03-11 16:58:14 +0100

    move playersValues to store; add useInterval hook; add updateValues function by dispatch action;

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
 import "./App.css";
-// import { playersActions } from "./store/index";
-import { useSelector, 
-    // useDispatch 
-} from "react-redux";
 import Players from "./components/Players/Players";
-import useInterval from './hooks/useInterval';
 import { useState } from 'react';
 
 const keysPressed = {};
 
 function App() {
     const [keysListenersReady, setKeysListenersReady] = useState(false);
-    // const dispatch = useDispatch();
-    const playersValues = useSelector(state => state.playersValues);
-    // const newPlayersValues = [...playersValues];
 
     const onKeyDown = e => {
         keysPressed[e.code] = 1;
@@ -26,27 +18,6 @@ function App() {
         document.addEventListener("keyup", onKeyUp);
         setKeysListenersReady(true);
     };
-    
-
-    playersValues.forEach((player, index) => {
-        const newValues = {...player.values};
-
-        if (keysPressed[player.keys.left]) {
-            console.log('left')
-            newValues.rotationSpeed = player.values.rotationSpeed - .1;
-        }
-        if (keysPressed[player.keys.rght]) {
-            console.log('right')
-            newValues.rotationSpeed = player.values.rotationSpeed + .1;
-        }
-        // newPlayersValues[index].values = newValues;
-        
-        return;
-    });
-
-    useInterval(() => {
-        // dispatch(playersActions.updatePlayersValues(newPlayersValues));
-    }, 1000);
 
     return (
         <div className="App" >
@@ -56,3 +27,5 @@ function App() {
 }
 
 export default App;
+
+export { keysPressed };
--- src/components/Players/Players.jsx
+++ src/components/Players/Players.jsx
@@ -1,9 +1,34 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect} from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { playersActions } from "../../store/index";
 import { StyledPlayer } from "./Player.styles";
+import { keysPressed } from '../../App';
+import useInterval from '../../hooks/useInterval';
+import { consts, validateRotationSpeed } from '../../helpers/index';
 
 const Players = () => {
+    const dispatch = useDispatch();
     const playersValues = useSelector(state => state.playersValues);
+    
+    const updatePlayersValues = () => {
+        let updatedValues = [];
+        playersValues.forEach(({id, values, keys}, index) => {
+            if (index === 0) {
+
+                const newValues = {...values};
+                newValues.rotationSpeed = validateRotationSpeed(values, keys);
+                newValues.angle += newValues.rotationSpeed;
+                console.log(newValues.rotationSpeed)
+                updatedValues[index] = {id, values: newValues, keys};
+            }
+        });
+
+        dispatch(playersActions.updatePlayersValues(updatedValues));
+    }
+
+    useInterval(() => {
+        updatePlayersValues();
+    }, consts.FRAME_INTERVAL);
 
     return playersValues.map((player, index) => {
         return <StyledPlayer values={player.values} key={index} id={index} />;
