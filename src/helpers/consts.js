93241a4b95e929e62525afb055f6d47d91334678
Marcin Ostaszewski <marcinostaszewski@gmail.com>
2022-03-13 13:07:04 +0100

    fix speed const

--- src/helpers/consts.js
+++ src/helpers/consts.js
@@ -3,7 +3,7 @@ const consts = {
     FRAME_INTERVAL: 24,
     DEG_TO_RAD: Math.PI / 180,
     RAD_TO_DEG: 180 / Math.PI,
-    SPEED_DELTA: 1,
+    SPEED_DELTA: .4,
     SPEED_MIN: .01,
     SPEED_MAX: 10,
     SPEED_DELTA_MAX: 5,
