bc4c74578fdd6b31b2b07f61d5d82f9f1bfc708b
Marcin Ostaszewski <marcinostaszewski@gmail.com>
2022-03-26 22:54:46 +0100

    add common colourPalette

---
+++ src/helpers/stylesCommons.js
@@ -0,0 +1,11 @@
+const coloursPalette = {
+    primary: '#4d784e',
+    primaryLight: '#6ea171',
+    secondary: '#675645',
+    secondaryLight: '#e1d798',
+    tertiary: '#424756'
+};
+
+export {
+    coloursPalette
+};
