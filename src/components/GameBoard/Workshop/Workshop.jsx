03271213506e2d3e9940100d9ec549e5fc55e662
marcin.ostaszewski <marcin.ostaszewski@wundermanthompson.com>
2022-04-08 22:43:28 +0200

    improve Workshop and health bar looks

--- src/components/GameBoard/Workshop/Workshop.jsx
+++ src/components/GameBoard/Workshop/Workshop.jsx
@@ -3,14 +3,16 @@ import { coloursPalette } from '../../../helpers/stylesCommons';
 import { repair } from '../../../assets/svg';
 
 const Workshop = props => {
-
     return (
         <div className="workshop" style={{
-            width: consts.PLAYER_RADIUS * 3,
-            height: consts.PLAYER_RADIUS * 3,
+            width: consts.PLAYER_RADIUS * consts.WORKSHOP_SCALE,
+            height: consts.PLAYER_RADIUS * consts.WORKSHOP_SCALE,
             backgroundColor: coloursPalette.primary,
             left: consts.WINDOW_WIDTH / 2,
             top: consts.WINDOW_HEIGHT / 2,
+            opacity: .7,
+            transform: `translate(-${consts.PLAYER_RADIUS * consts.WORKSHOP_SCALE / 2}px, 
+                -${consts.PLAYER_RADIUS * consts.WORKSHOP_SCALE / 2}px)`
         }}>
             {repair}
         </div>
