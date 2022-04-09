94e75eb8ae0c52fc7c500b33dc7a9f796af54d9b
marcin.ostaszewski <marcin.ostaszewski@wundermanthompson.com>
2022-04-09 13:07:00 +0200

    improve information on Welcome screen

--- src/components/GameStateView/Welcome.Styled.jsx
+++ src/components/GameStateView/Welcome.Styled.jsx
@@ -11,6 +11,21 @@ const WelcomeStyled = styled.div`
     h1 {
         ${headers.h1}
     }
+    h2 {
+        font-size: 20px;
+        font-weight: bold;
+        padding: 30px 0 20px;
+        text-decoration: underline;
+    }
+    p {
+        padding: 5px 0;
+    }
+    ul {
+        list-style: disc;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
 `;
 
 export default WelcomeStyled;
--- src/components/GameStateView/Welcome.jsx
+++ src/components/GameStateView/Welcome.jsx
@@ -6,8 +6,18 @@ export default function Welcome() {
         <WelcomeStyled>
             <p>Welcome to</p>
             <h1>Sliding Tanks</h1>
-            <p>set game properties in <strong>Settings</strong></p><br />
+            <p>set game properties in <strong>Settings</strong></p>
             <p>then you can <strong>Start</strong> and <strong>Pause</strong> the game</p>
+
+            <h2>The rules are simple:</h2>
+            <ul>
+                <li><p>Collect <strong>Stars</strong> for points</p></li>
+                <li><p>Stars will <strong>get smaller</strong> each time</p></li>
+                <li><p><strong>Collisions damage</strong> your tank</p></li>
+                <li><p>Visiting <strong>Workshop repairs</strong> your tank slowly</p></li>
+                <li><p><strong>Repair is faster</strong> when closer to the center</p></li>
+            </ul>
+
         </WelcomeStyled>
     )
 }
