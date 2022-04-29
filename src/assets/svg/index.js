2bd391be4bfa7350ad68b5e2dc3a83e495bdfcd9
marcin.ostaszewski <marcin.ostaszewski@wundermanthompson.com>
2022-04-29 22:36:05 +0200

    add new bonus icons

--- src/assets/svg/index.js
+++ src/assets/svg/index.js
@@ -2,10 +2,14 @@ import { heart } from './heart';
 import { star } from './star';
 import { repair } from './repair';
 import { mine } from './mine';
+import { bullet } from './bullet';
+import { missile } from './missile';
 
 export {
     heart,
     star,
     repair,
-    mine
+    mine,
+    bullet,
+    missile
 };
