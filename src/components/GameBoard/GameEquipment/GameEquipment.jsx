4d21c73303c71db9b8ef913c2e48121bf1fd3149
marcin.ostaszewski <marcin.ostaszewski@wundermanthompson.com>
2022-05-23 21:51:51 +0200

    add display equipment on GameBoard

---
+++ src/components/GameBoard/GameEquipment/GameEquipment.jsx
@@ -0,0 +1,20 @@
+import { mine } from '../../../assets/svg';
+
+const GameEquipment = (props) => {
+    const mines = props.onBoardEquipment.mines.map((position, index) => (
+        <div
+            key={index}
+            className="mine"
+            style={{ left: position.x, top: position.y }}>
+            {mine}
+        </div>
+    ));
+
+    return (
+        <div className="on-board-equipment">
+            {mines}
+        </div>
+    );
+}
+
+export default GameEquipment;
