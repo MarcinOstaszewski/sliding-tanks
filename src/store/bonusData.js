122d34c6060ebf9dc4a7f59b7d7ac77cece948e7
marcin.ostaszewski <marcinostaszewski@gmail.com>
2022-10-20 22:14:02 +0200

    add bullet to bonus data

--- src/store/bonusData.js
+++ src/store/bonusData.js
@@ -1,16 +1,22 @@
 import { getRandomBetween, getRandomPosition } from '../helpers';
 
-const bonusTypes = {
-    0: 'mine',
-    1: 'repair'
-    // 2: 'bullet',
+export const bonusNames = {
+    MINE: 'mine',
+    REPAIR: 'repair',
+    BULLET: 'bullet',
+}
+
+const activeBonusArrays = {
+    0: bonusNames.MINE,
+    1: bonusNames.REPAIR,
+    2: bonusNames.BULLET,
     // 3: 'missile'
 }
 
 export const resetBonusValues = () => ({
     position: getRandomPosition('upper'),
     speed: { x: 0, y: getRandomBetween(1, 2.5) },
-    bonusType: bonusTypes[Math.floor(getRandomBetween(0, Object.keys(bonusTypes).length))]
+    bonusType: activeBonusArrays[Math.floor(getRandomBetween(0, Object.keys(activeBonusArrays).length))]
 });
 
 export const bonusData = resetBonusValues();
